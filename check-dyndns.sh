5c6855bd2449cfea6bbabb79e42ea1368b787200
Tim Howard <thoward@redhat.com>
2020-06-01 20:57:22 +0700

    Add more verification

--- check-dyndns.sh
+++ check-dyndns.sh
@@ -3,15 +3,22 @@
 # $1 is the A name record to check. ie myname.hopto.org
 
 URL=$1
-DNS_IP=`dig +short uluvus.hopto.org`
+
+echo $1
+if [ $URL = ""]
+then
+read -p "Please enter the URL you want to check: " URL
+fi
+
+DNS_IP=`dig +short $URL`
 NAT_IP=`dig +short myip.opendns.com @resolver1.opendns.com`
 
-echo 'The URL we are check is: '$URL
-echo 'The dns host record for '$URL 'is: '$DNS_IP
+echo 'The URL we are checking is: '$URL
+echo 'The dns host record for '$URL' is: '$DNS_IP
 echo 'The NAT IP  detected is: '$NAT_IP
-if [ $DNS = $NAT_IP ]
+if [ $DNS_IP = $NAT_IP ]
 then
-    echo 'Yes, the IP for ' $URL'matches'
+    echo 'Yes, the IP for '$URL' matches'
     exit 0
 else
     echo  'Sorry the IPs do not match. Please update your dyndns'
