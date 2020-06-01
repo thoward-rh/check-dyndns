9afbfcb7d3df714fb5341210689301b8a2422cba
Tim Howard <thoward@redhat.com>
2020-06-01 21:41:29 +0700

    Update with fixes

--- check-dyndns.sh
+++ check-dyndns.sh
@@ -2,27 +2,28 @@
 
 # $1 is the A name record to check. ie myname.hopto.org
 
-URL=$1
+HOSTNAME=$1
+# Prompt for input if no host name is given.
+while [[ $HOSTNAME = "" ]]
+do
+read -p "Please enter the URL you want to check: " HOSTNAME
+done
 
-echo $1
-if [ $URL = ""]
-then
-read -p "Please enter the URL you want to check: " URL
-fi
-
-DNS_IP=`dig +short $URL`
+# Get the IPs we need to check.
+DNS_IP=`dig +short $HOSTNAME`
 NAT_IP=`dig +short myip.opendns.com @resolver1.opendns.com`
 
-echo 'The URL we are checking is: '$URL
-echo 'The dns host record for '$URL' is: '$DNS_IP
+# Print the details of what we are checking.
+echo 'The URL we are checking is: '$HOSTNAME
+echo 'The dns host record for '$HOSTNAME' is: '$DNS_IP
 echo 'The NAT IP  detected is: '$NAT_IP
 if [ $DNS_IP = $NAT_IP ]
 then
-    echo 'Yes, the IP for '$URL' matches'
+    echo 'Yes, the IP for '$HOSTNAME' matches'
     exit 0
 else
-    echo  'Sorry the IPs do not match. Please update your dyndns'
+    echo  'Sorry the IPs do not match. Please check your host name is correct or update your dyndns.'
     exit 1
 fi
 
-exit 0
+exit 1
