4a14877fd33c870f603e6089aa9c8c2609f802b6
Tim Howard <thoward@redhat.com>
2020-06-01 20:41:25 +0700

    Changes

--- check-dyndns.sh
+++ check-dyndns.sh
@@ -6,17 +6,16 @@ URL=$1
 DNS_IP=`dig +short uluvus.hopto.org`
 NAT_IP=`dig +short myip.opendns.com @resolver1.opendns.com`
 
-if [ $URL = $NAT_IP ]
 echo 'The URL we are check is: '$URL
 echo 'The dns host record for '$URL 'is: '$DNS_IP
 echo 'The NAT IP  detected is: '$NAT_IP
+if [ $DNS = $NAT_IP ]
 then
-    echo "Yes, the IP matches"
+    echo 'Yes, the IP for ' $URL'matches'
     exit 0
 else
-    echo  "Sorry the IP do not match. Please update your dyndns"
+    echo  'Sorry the IPs do not match. Please update your dyndns'
     exit 1
-
 fi
 
 exit 0
